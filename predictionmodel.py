09f40a311f2a5d449701d60a79b8fe68141e7c7f
Brandon Finley <brfi3983@colorado.edu>
2021-02-04 16:55:50 -0700

    added prediction model

---
+++ predictionmodel.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# ========================================================
+def main():
+	data = np.genfromtxt('2021MCM_ProblemC__Images_by_GlobalID.xlsx', delimiter = ',')
+
+# ========================================================
+if __name__ == "__main__":
+	main()
